128bb56e653007a147429001af0380a23cc9c3c2
Home <Home@MacBook-Pro.local>
2019-08-29 16:39:19 -0700

    circletest

--- database/config.js
+++ database/config.js
@@ -9,7 +9,7 @@ const createPool = () => {
         connectionLimit: 100,
         host: 'localhost',
         user: 'root',
-        password: 'ACisdude5',
+        password: 'circlepswd',
         database: DATABASE,
         multipleStatements: process.env.NODE_ENV === 'test',
         charset: 'utf8mb4'
