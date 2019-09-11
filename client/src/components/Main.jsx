1f0be3c518783e4f9c308df5968a55eb779ee8ee
Home <Home@MacBook-Pro.local>
2019-09-11 12:13:04 -0700

    dt

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -20,7 +20,7 @@ class Main extends React.Component {
         return (
             <div styleName="main-container" data-testid="main-component">
                 <div>
-                    <p>does this work 2?</p>
+                    <p>does this work 3?</p>
                 </div>
                 <div styleName="flipper-container">
                     <ReactCardFlip
